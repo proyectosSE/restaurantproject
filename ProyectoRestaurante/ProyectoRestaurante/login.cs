39c7d20a6654a39f227c8deedb334d286682f7b9
LIKELIAS <165118365+LIKELIAS@users.noreply.github.com>
2024-08-02 18:18:33 -0400

    Add files via upload

---
+++ ProyectoRestaurante/ProyectoRestaurante/login.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ProyectoRestaurante
+{
+    public partial class login : Form
+    {
+        public static SqlConnection conexion = new SqlConnection("server=ELIASPC;Database=restaurantpj;Integrated Security=true");
+        
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+    }
+}

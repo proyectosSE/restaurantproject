d4044d7ab6dafbd15b85f6793557d2af76e199f4
LIKELIAS <165118365+LIKELIAS@users.noreply.github.com>
2024-08-08 13:06:52 -0400

    Add files via upload
    ACTE

---
+++ ProyectoRestaurante/ProyectoRestaurante/mantenimientos/mantmesas.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoRestaurante
+{
+    public partial class mantmesas : Form
+    {
+        public mantmesas()
+        {
+            InitializeComponent();
+            fechamesa.Format = DateTimePickerFormat.Short;
+            buttEdit.Visible = false;
+            cargardatos();
+        }
+
+        private string mivar;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Conectar cls = new Conectar();
+            string datos = ""+cbbsala.SelectedValue+",'"+txtmesa.Text+"',"+txtasientos.Text+",'"+fechamesa.Text+"',"+txtestado.Text+"";
+            string tabla = "mesas";
+            cls.Agregar(datos, tabla);
+            cargardatos();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mantmesas_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'proyectoRestauranteDataSet16.salas' Puede moverla o quitarla según sea necesario.
+            this.salasTableAdapter4.Fill(this.proyectoRestauranteDataSet16.salas);
+
+        }
+        private void cargardatos()
+        {
+            String consulta = "SELECT * FROM mesas";
+            
+            try
+            {
+                using (SqlConnection conexion = new SqlConnection(rutadb.conexion))
+                {
+                    conexion.Open();
+
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(consulta, conexion))
+                    {
+                        DataTable dt = new DataTable();
+                        adapter.Fill(dt);
+                        dataGridView1.DataSource = dt;
+
+                        // Configuración de columnas
+                        if (dataGridView1.Columns.Contains("id_mesa")) dataGridView1.Columns["id_mesa"].DisplayIndex = 0;
+                        if (dataGridView1.Columns.Contains("id_sala")) dataGridView1.Columns["id_sala"].DisplayIndex = 1;
+                        if (dataGridView1.Columns.Contains("nommesa")) dataGridView1.Columns["nommesa"].DisplayIndex = 2;
+                        if (dataGridView1.Columns.Contains("puestos")) dataGridView1.Columns["puestos"].DisplayIndex = 3;
+                        if (dataGridView1.Columns.Contains("fecha")) dataGridView1.Columns["fecha"].DisplayIndex = 4;
+                        if (dataGridView1.Columns.Contains("statusmesa")) dataGridView1.Columns["statusmesa"].DisplayIndex = 5;
+                        if (dataGridView1.Columns.Contains("editar")) dataGridView1.Columns["editar"].DisplayIndex = 6;
+                        if (dataGridView1.Columns.Contains("eliminar")) dataGridView1.Columns["eliminar"].DisplayIndex = 7;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Manejo de errores
+                MessageBox.Show("Error al cargar datos: " + ex.Message);
+            }
+
+        }
+
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.Columns[e.ColumnIndex].Name == "Editar")
+            {
+                cbbsala.SelectedValue = dataGridView1.CurrentRow.Cells["id_sala"].Value.ToString();
+                txtmesa.Text = dataGridView1.CurrentRow.Cells["nommesa"].Value.ToString();
+                txtasientos.Text = dataGridView1.CurrentRow.Cells["puestos"].Value.ToString();
+                fechamesa.Text = dataGridView1.CurrentRow.Cells["fecha"].Value.ToString();
+                txtestado.Text = dataGridView1.CurrentRow.Cells["statusmesa"].Value.ToString();
+                mivar = dataGridView1.CurrentRow.Cells["id_mesa"].Value.ToString();
+                buttEdit.Visible = true;
+                button1.Visible = false;
+
+            }
+            if (dataGridView1.Columns[e.ColumnIndex].Name == "Eliminar")
+            {
+                mensaje ms = new mensaje();
+
+                if (ms.ShowDialog() == DialogResult.OK)
+                {
+                    mivar = dataGridView1.CurrentRow.Cells["id_mesa"].Value.ToString();
+                    Conectar cls = new Conectar();
+                    string tbl = "mesas";
+                    string id = "id_mesa = '" + mivar + "'";
+                    cls.eliminar(tbl, id);
+                    cargardatos();
+                }
+            }
+        }
+
+        private void buttEdit_Click(object sender, EventArgs e)
+        {
+            Conectar cls = new Conectar();
+            string up = "id_sala ='" + cbbsala.SelectedValue + "',nommesa = "+"'"+ txtmesa.Text+ "',puestos ="+txtasientos.Text+", fecha ="+"'"+fechamesa.Text+"', statusmesa ='"+txtestado.Text+"'";
+            string tbl = "mesas";
+            string id = "id_mesa = '" + mivar + "'";
+            cls.Actualizar(up, tbl, id);
+
+            buttEdit.Visible = false;
+            cargardatos();
+            button1.Visible = true;
+        }
+
+        private void btcerrar_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ restaurantpj/Login.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace restaurantpj
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            trackBar1 = new TrackBar();
+            label2 = new Label();
+            label3 = new Label();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(247, 167);
+            button1.Name = "button1";
+            button1.Size = new Size(273, 127);
+            button1.TabIndex = 0;
+            button1.Text = "prueba";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(362, 99);
+            label1.Name = "label1";
+            label1.Size = new Size(53, 25);
+            label1.TabIndex = 1;
+            label1.Text = "waos";
+            // 
+            // trackBar1
+            // 
+            trackBar1.Location = new Point(247, 352);
+            trackBar1.Name = "trackBar1";
+            trackBar1.Size = new Size(273, 69);
+            trackBar1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(467, 99);
+            label2.Name = "label2";
+            label2.Size = new Size(53, 25);
+            label2.TabIndex = 3;
+            label2.Text = "waos";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(256, 99);
+            label3.Name = "label3";
+            label3.Size = new Size(53, 25);
+            label3.TabIndex = 4;
+            label3.Text = "waos";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(trackBar1);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private TrackBar trackBar1;
+        private Label label2;
+        private Label label3;
+    }
+}
